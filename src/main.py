f13f9d2af3ea255c4542368bd10d4fa7580246ac
niblit <85725939+niblit@users.noreply.github.com>
2023-08-06 20:58:04 -0600

    Added board drawing function

--- src/main.py
+++ src/main.py
@@ -1,16 +1,22 @@
 import pygame
 
 FPS = 60
-SCREEN_SIZE = WIDTH, HEIGHT = (600, 600)
+
+SCREEN_SIZE = 600
+CELL_SIZE = SCREEN_SIZE / 8
+
 BACKGROUND = (0, 0, 0)
+LIGHT_CELL = (240, 217, 181)
+DARK_CELL = (181, 136, 99)
+
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE)
+    screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
     pygame.display.set_caption("Checkers")
     clock = pygame.time.Clock()
-    running = True
 
+    running = True
     while running:
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
@@ -21,6 +27,7 @@ def main():
         screen.fill(BACKGROUND)
 
         # RENDER YOUR GAME HERE
+        draw_board(screen)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
@@ -28,6 +35,14 @@ def main():
         clock.tick(FPS)  # limits FPS
 
     pygame.quit()
+
+
+def draw_board(screen):
+    for y in range(8):
+        for x in range(8):
+            color = LIGHT_CELL if (x + y) % 2 == 0 else DARK_CELL
+            pygame.draw.rect(screen, color, (x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
 
 if __name__ == "__main__":
     main()
